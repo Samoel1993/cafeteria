255fc55d48e1b74261dde01bc9f05b2c1c33be75
Samoel <samoel.jrr@gmail.com>
2024-11-14 20:27:43 -0300

    Alterando pra Teste

--- src/cafeteria/vendas/produtos/ProdutoView.java
+++ src/cafeteria/vendas/produtos/ProdutoView.java
@@ -111,7 +111,7 @@ public class ProdutoView extends JInternalFrame {
 		btNovoProduto.setBounds(400, 35, 139, 27);
 		getContentPane().add(btNovoProduto);
 
-		btPesquisar = new JButton("Teste");
+		btPesquisar = new JButton("Pesquisa");
 		btPesquisar.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
